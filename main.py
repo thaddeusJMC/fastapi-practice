b09f8afc0ccdc30ed8103093643933dd99e8689b
Thad <thad@meowwwcorp.com>
2024-01-28 20:02:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from supabase import create_client
+from decouple import config
+import os
+
+app = FastAPI()
+url = config("SUPABASE_URL")
+key = config("SUPABASE_KEY")
+
+@app.get("/invoices")
+async def root():
+  supa = create_client(url, key)
+
+  return supa.from_("invoices").select("*").execute()
+
+@app.get("/customers")
+async def root():
+  supa = create_client(url, key)
+
+  return supa.from_("customers").select("*").execute()
